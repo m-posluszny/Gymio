5c3e20b0ceba1b5696d52c2d60099a7811208fc9
Steampunk539 <nfitowska@gmail.com>
2021-03-27 22:05:35 +0100

    Added htmls

---
+++ static/js/index.js
@@ -0,0 +1,15 @@
+var name = document.querySelector('name');
+var room = document.getElementById('room');
+var join = document.getElementById('join');
+var join = document.getElementById('create');
+
+
+function join(){
+    var name = document.getElementById("name");
+    var name = document.getElementById("room");
+}
+
+function create(){
+    var name = document.getElementById("name");
+    var name = document.getElementById("room");
+}
