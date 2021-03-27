debe7a55f60d3871628a9b884fcd4d7782d2818c
m-posluszny <mposluszny.foto@gmail.com>
2021-03-27 17:36:46 +0100

    placeholder functionaolty

--- server.py
+++ server.py
@@ -1,28 +1,26 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit,  join_room, leave_room
 
 # Creating a flask app and using it to instantiate a socket object
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-# values['slider1'] and values['slider2'] store the current value of the sliders
-# This is done to prevent data loss on page reload by client.
-values = {
-    'slider1': 25,
-    'slider2': 0,
-}
 
-# Handler for default flask route
-# Using jinja template to render html along with slider value as input
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    channel = data['channel']
+    join_room(channel)
+
 @app.route('/')
 def index():
-    return render_template('index.html',**values)
+    return render_template('index.html')
 
-# Handler for a message recieved over 'connect' channel
 @socketio.on('connect')
 def test_connect():
     emit('after connect',  {'data':'Lets dance'})
+    print('connected')
 
-# Notice how socketio.run takes care of app instantiation as well.
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0',port=5000)
